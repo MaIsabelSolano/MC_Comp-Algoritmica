a71b106bdc8ea398e4212445c034d149130458e7
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2024-08-29 18:17:09 -0600

    Referencias

--- Rain_v1.py
+++ Rain_v1.py
@@ -6,6 +6,12 @@
 
 from music import *
 
+## REFERENCES ===============================================
+# https://www.youtube.com/watch?v=rZLNxpz5bfY (trombon)
+# https://musescore.com/user/9026011/scores/2688026 (notas de abajo)
+# https://musescore.com/user/29706205/scores/6033100 (alternativa)
+
+
 score = Score()
 
 
@@ -16,10 +22,17 @@ mainPianoPhrase.setTempo(190);
 
 # notas
 pitches1   = [A3, G4, C5,       B4, G4, REST]
-durations1 = [QN, QN, DQN, QN + EN, HN,   QN]
+durations1 = [QN, QN, DQN, QN + EN, HN,   EN]
+pitches2   = [A3, G4, C5,       B4, REST, REST, REST]
+durations2 = [QN, QN, DQN, QN + QN,  SN,    EN,   QN]
+pitches3   = [E4, E4, D4, D4, E4, C4]
+durations3 = [QN, QN, QN, EN, QN, EN]
 
 ## agregar
 mainPianoPhrase.addNoteList(pitches1, durations1)
+mainPianoPhrase.addNoteList(pitches1, durations1)
+mainPianoPhrase.addNoteList(pitches2, durations2)
+mainPianoPhrase.addNoteList(pitches3, durations3)
 pianoPart.addPhrase(mainPianoPhrase)
 score.addPart(pianoPart)
 
