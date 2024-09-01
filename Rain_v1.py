da429bb40d4e1798d6cd84ace7ad982ac8e3cef6
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2024-09-01 03:55:33 -0600

    Primera parte

--- Rain_v1.py
+++ Rain_v1.py
@@ -55,6 +55,77 @@ DDD11 = [DQN, EN, HN,   QN,   EN, QN]
 PPP12 = [F4, E4, D4, E4,      F4, REST, F4]
 DDD12 = [QN, QN, QN, EN, EN + HN,   EN, QN]
 
+PPP13 = [G4, F4, E4, F4,      G4, REST]
+DDD13 = [QN, QN, QN, EN, EN + HN,   EN]
+
+PPP14 = [REST, A4, A4,      A4, E4, F4, A4]
+DDD14 = [  EN, QN, EN, EN + QN, QN, QN, QN]
+
+PPP15 = [A4, GS4,      E4, REST, E4]
+DDD15 = [QN, EN, EN + EN,   EN, QN]
+
+PPP16 = [G4, F4, E4, F4,      G4, REST, G4, G4]
+DDD16 = [QN, QN, QN, EN, EN + HN,   EN, EN, EN]
+
+PPP17 = [A4, A4,      A4, REST, A4]
+DDD17 = [QN, EN, EN + EN,   SN, QN]
+
+PPP18 = [GS4, E4,  G4,      A4, REST, REST, REST]
+DDD18 = [ QN, QN, DQN, EN + QN,   QN,   HN,   WN]
+
+PPP19 = [C5, C5, C5,      C5, F4,      C5, REST, REST, C5, C5, C5]
+DDD19 = [EN, EN, EN, EN + EN, QN, EN + QN,   QN,   SN, EN, EN, EN]
+
+PPP20 = [C5, B4, B4,      B4, E4,      C5, B4, A4, REST, E4, E4]
+DDD20 = [EN, EN, EN, EN + EN, QN, EN + EN, EN, QN,   QN, EN, EN]
+
+PPP21 = [F4,       G4, A4, G4, D4, E4, F4,  F4, E4, E4, REST]
+DDD21 = [DQN, EN + EN, QN, QN, QN, QN, QN, DQN, EN, HN,   WN]
+
+PPP22 = [C5, C5, C5,      C5, F4,      C5, REST, REST, C5, D5, C5]
+DDD22 = [EN, EN, EN, EN + EN, QN, EN + QN,   QN,   SN, EN, EN, EN]
+
+PPP23 = [C5, B4, B4,      B4, E4,      C5, B4, A4, REST, E4]
+DDD23 = [EN, EN, EN, EN + EN, QN, EN + EN, EN, QN,   QN, EN]
+
+PPP24 = [ F4,      G4,      A4, REST, A4, B4, A4]
+DDD24 = [DQN, EN + QN, QN + HN,   EN, EN, EN, EN]
+
+PPP25 = [GS4,      E4,      B4, REST]
+DDD25 = [DQN, EN + QN, QN + HN,   HN]
+
+PPP26 = [E4, REST,     GS5, REST, A4]
+DDD26 = [QN,   EN, EN + EN,   EN, QN]
+
+PPP27 = [REST,  B4, A4, E5, D5, REST, C5, D5, E5, G5, E5, D5, C5]
+DDD27 = [  EN, DQN, QN, QN, QN,   QN, QN, QN, QN, EN, EN, QN, QN]
+
+PPP28 = [B4,       C5, REST, A4, E5]
+DDD28 = [DHN, QN + QN,   QN, QN, QN]
+
+PPP29 = [D5, REST, A4, E5, D5, D5, C5, B4]
+DDD29 = [QN,   QN, QN, QN, QN, QN, QN, QN]
+
+PPP30 = [B4, B4,      C5, REST, A4, E5]
+DDD30 = [HN, QN, QN + QN,   QN, QN, QN]
+
+PPP31 = [D5, REST, C5, D5, E5, G5, E5, D5, C5]
+DDD31 = [QN,   QN, QN, QN, QN, EN, EN, QN, QN]
+
+PPP32 = [B4, B4, B4, D5,      C5, REST, A4]
+DDD32 = [QN, QN, QN, EN, EN + HN,   QN, QN]
+
+PPP33 = [E5,      A4,      D5, C5, REST, A4]
+DDD33 = [DQN, EN + QN, QN + QN, QN,   QN, QN]
+
+PPP34 = [E5,       A4,      G5, E5, D5, D5, E5,       E5]
+DDD34 = [DQN, EN + QN, QN + QN, QN, QN, QN, QN, DHN + WN]
+
+PPP35 = [REST, REST, E4]
+DDD35 = [  HN,   QN, QN]
+
+PPP36 = [G4,       A4,      A4, REST]
+DDD36 = [DQN, EN + QN, QN + HN,   HN]
 
 
 ## agregar
@@ -73,10 +144,51 @@ mainPianoPhrase.addNoteList(PPP9, DDD9)
 mainPianoPhrase.addNoteList(PPP10, DDD10)
 mainPianoPhrase.addNoteList(PPP11, DDD11)
 mainPianoPhrase.addNoteList(PPP12, DDD12)
+mainPianoPhrase.addNoteList(PPP13, DDD13)
+mainPianoPhrase.addNoteList(PPP14, DDD14)
+mainPianoPhrase.addNoteList(PPP15, DDD15) # **
+mainPianoPhrase.addNoteList(PPP7, DDD7)
+mainPianoPhrase.addNoteList(PPP8, DDD8)
+mainPianoPhrase.addNoteList(PPP7, DDD7)
+mainPianoPhrase.addNoteList(PPP9, DDD9)
+mainPianoPhrase.addNoteList(PPP10, DDD10)
+mainPianoPhrase.addNoteList(PPP11, DDD11)
+mainPianoPhrase.addNoteList(PPP12, DDD12)
+mainPianoPhrase.addNoteList(PPP16, DDD16)
+mainPianoPhrase.addNoteList(PPP17, DDD17)
+mainPianoPhrase.addNoteList(PPP18, DDD18)
+mainPianoPhrase.addNoteList(PPP19, DDD19)
+mainPianoPhrase.addNoteList(PPP20, DDD20)
+mainPianoPhrase.addNoteList(PPP21, DDD21)
+
+mainPianoPhrase.addNoteList(PPP22, DDD22)
+mainPianoPhrase.addNoteList(PPP23, DDD23)
+mainPianoPhrase.addNoteList(PPP24, DDD24)
+mainPianoPhrase.addNoteList(PPP25, DDD25)
+
+mainPianoPhrase.addNoteList(PPP26, DDD26)
+mainPianoPhrase.addNoteList(PPP27, DDD27)
+mainPianoPhrase.addNoteList(PPP28, DDD28)
+mainPianoPhrase.addNoteList(PPP29, DDD29)
+mainPianoPhrase.addNoteList(PPP30, DDD30)
+mainPianoPhrase.addNoteList(PPP31, DDD31)
+mainPianoPhrase.addNoteList(PPP32, DDD32)
+
+mainPianoPhrase.addNoteList(PPP33, DDD33)
+mainPianoPhrase.addNoteList(PPP34, DDD34)
+
+mainPianoPhrase.addNoteList(PPP35, DDD35)
+mainPianoPhrase.addNoteList(PPP7, DDD7)
+mainPianoPhrase.addNoteList(PPP8, DDD8)
+mainPianoPhrase.addNoteList(PPP7, DDD7)
+mainPianoPhrase.addNoteList(PPP36, DDD36)
+
+
 
 pianoPart.addPhrase(mainPianoPhrase)
 score.addPart(pianoPart)
 
 
 # play it
+# View.sketch(score)
 Play.midi(score)
