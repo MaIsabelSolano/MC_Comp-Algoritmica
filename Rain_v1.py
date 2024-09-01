201a787a827445de7fbc53156836258e49ff0d7e
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2024-09-01 02:06:36 -0600

    Arreglo, cambio de hn a en. Avances

--- Rain_v1.py
+++ Rain_v1.py
@@ -26,13 +26,13 @@ PPP2  = [A3, G4, C5,       B4, REST, REST, REST]
 DDD2  = [QN, QN, DQN, QN + QN,  SN,    EN,   QN]
 
 PPP3  = [E4, E4,      D4, E4,      C4, E4,      B3, D4]
-DDD3  = [QN, HN, HN + EN, QN, EN + EN, QN, EN + EN, DQN]
+DDD3  = [QN, EN, EN + EN, QN, EN + EN, QN, EN + EN, DDQN]
 
 PPP4  = [E4, E4,      D4, E4,      C4, D4,       G4, REST]
-DDD4  = [QN, HN, HN + EN, QN, EN + EN, QN, EN + DQN,   SN]
+DDD4  = [QN, EN, EN + EN, QN, EN + EN, QN, EN + DQN,   SN]
 
 PPP5  = [E4, E4,      D4, E4,      C4, D4,      G4, D4, C4]
-DDD5  = [QN, HN, HN + EN, QN, EN + EN, QN, EN + EN, QN, EN]
+DDD5  = [QN, EN, EN + EN, QN, EN + EN, QN, EN + EN, QN, EN]
 
 PPP6  = [C4, G4,      D4, G4, D4,      E4, REST, E4]
 DDD6  = [HN, QN, QN + QN, QN, QN, QN + HN,   EN, QN]
@@ -46,8 +46,14 @@ DDD8  = [DQN, EN + EN,   SN, QN]
 PPP9  = [G4,       A4, G4, A4, REST]
 DDD9  = [DQN, EN + QN, QN, DHN,  EN]
 
-PPP10 = [REST, G4, G4]
-DDD10 = [  QN, HN, HN]
+PPP10 = [REST, G4, G4,      G4, D4, E4, F4]
+DDD10 = [  QN, QN, EN, EN + EN, QN, QN, QN]
+
+PPP11 = [ F4, E4, E4, REST, REST, E4]
+DDD11 = [DQN, EN, HN,   QN,   EN, QN]
+
+PPP12 = [F4, E4, D4, E4,      F4, REST, F4]
+DDD12 = [QN, QN, QN, EN, EN + HN,   EN, QN]
 
 
 
@@ -64,6 +70,9 @@ mainPianoPhrase.addNoteList(PPP7, DDD7)
 mainPianoPhrase.addNoteList(PPP8, DDD8)
 mainPianoPhrase.addNoteList(PPP7, DDD7)
 mainPianoPhrase.addNoteList(PPP9, DDD9)
+mainPianoPhrase.addNoteList(PPP10, DDD10)
+mainPianoPhrase.addNoteList(PPP11, DDD11)
+mainPianoPhrase.addNoteList(PPP12, DDD12)
 
 pianoPart.addPhrase(mainPianoPhrase)
 score.addPart(pianoPart)
